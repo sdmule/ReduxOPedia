72343f186a962bbeccfd7aced7ce7ecefeb183a7
sdmule <mulesaurabh45@gmail.com>
2026-07-29 13:02:29 +0530

    App Layout

--- reduxopedia/src/main.jsx
+++ reduxopedia/src/main.jsx
@@ -1,6 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>hello</StrictMode>,
+  <StrictMode>
+    <div className="d-flex flex-column min-vh-100">
+      <Header />
+      <main className="flex-grow-1">Main Content</main>
+      <Footer />
+    </div>
+  </StrictMode>,
 );
